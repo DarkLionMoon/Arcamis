79318c0ffeb9c7a288f9122d65b80d4a3c50bf8b
DarkLionMoon <82204205+DarkLionMoon@users.noreply.github.com>
2026-04-11 18:25:41 +0200

    Update notion.js

--- functions/api/notion.js
+++ functions/api/notion.js
@@ -260,6 +260,7 @@ async function fetchDb(cleanId, headers) {
     allResults = allResults.concat(data.results);
     cursor = data.has_more ? data.next_cursor : null;
   } while (cursor);
+  allResults = allResults.filter(p => p.object === 'page');
 
   if (cleanId === TIMELINE_DB.replace(/-/g, '')) {
     allResults.sort(function(a, b) {
