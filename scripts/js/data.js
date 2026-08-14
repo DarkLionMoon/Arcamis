90caa2272430d1b2776156660d547372edda548f
GandoraX-G <briangodwin615@gmail.com>
2026-08-14 20:05:00 +0200

    admin: add page pantheon to registry

--- scripts/js/data.js
+++ scripts/js/data.js
@@ -25,7 +25,8 @@ var pages = [
   {k:'maestria',   l:'Maestria / Titoli',       i:'🔨', id:'2f00274fdc1c802a9babd4239d97a319'},
   {k:"la-storia-di-gandora", l:"La Storia di Gandora", i:"🐉", id:"pag-la-storia-di-gandora", sec:"lore", sub:"Storia"},
   {k:"regole-del-server", l:"Regole del Server", i:"📋", id:"pag-regole-del-server", sec:"regole"},
-  {k:"materiale-approvato", l:"Materiale Approvato", i:"📖", id:"pag-materiale-approvato", sec:"personaggio", sub:"Regole"}
+  {k:"materiale-approvato", l:"Materiale Approvato", i:"📖", id:"pag-materiale-approvato", sec:"personaggio", sub:"Regole"},
+  {k:"pantheon", l:"Pantheon", i:"🛐", id:"pag-pantheon", sec:"lore"},
 ];
 
 function getPage(idOrK){
